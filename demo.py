6c2f6be36630b0483d834e585b1e6b9e7083e2dd
springyboiii <sugumararvinth19991018@gmail.com>
2023-09-19 08:45:17 +0530

    initialized repository

---
+++ demo.py
@@ -0,0 +1,192 @@
+import argparse
+import time
+from pathlib import Path
+import cv2
+import torch
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+import os 
+# Conclude setting / general reprocessing / plots / metrices / datasets
+from utils.utils import \
+    time_synchronized,select_device, increment_path,\
+    scale_coords,xyxy2xywh,non_max_suppression,split_for_trace_model,\
+    driving_area_mask,lane_line_mask,plot_one_box,show_seg_result,get_drivable_area_in_1D,\
+    AverageMeter,\
+    LoadImages
+
+
+
+
+def make_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='data/weights/yolopv2.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='data/example.jpg', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='cpu', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    return parser
+
+
+def detect():
+    # setting and directories
+    source, weights,  save_txt, imgsz = opt.source, opt.weights,  opt.save_txt, opt.img_size
+    save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
+
+    save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    inf_time = AverageMeter()
+    waste_time = AverageMeter()
+    nms_time = AverageMeter()
+
+    # Load model
+    stride =32
+    model  = torch.jit.load(weights, map_location='cpu')
+    device = select_device(opt.device)
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+    model = model.to(device)
+
+    if half:
+        model.half()  # to FP16  
+    model.eval()
+
+    # Set Dataloader
+    vid_path, vid_writer = None, None
+    dataset = LoadImages(source, img_size=imgsz, stride=stride)
+
+    prev_frames = []
+    prev_10_frames = []
+    next_5_frames = []
+    
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    t0 = time.time()
+    for path, img, im0s, vid_cap in dataset:
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Inference
+        t1 = time_synchronized()
+        [pred,anchor_grid],seg,ll= model(img)
+        t2 = time_synchronized()
+
+        # waste time: the incompatibility of  torch.jit.trace causes extra time consumption in demo version 
+        # but this problem will not appear in offical version 
+        tw1 = time_synchronized()
+        pred = split_for_trace_model(pred,anchor_grid)
+        tw2 = time_synchronized()
+
+        # Apply NMS
+        t3 = time_synchronized()
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        t4 = time_synchronized()
+
+        da_seg_mask = driving_area_mask(seg)
+        # ll_seg_mask = lane_line_mask(ll)
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+
+            p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+
+            # print("frame", frame)
+
+            p = Path(p)  # to Path
+            save_path = str(save_dir / p.name)  # img.jpg
+            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+            s += '%gx%g ' % img.shape[2:]  # print string
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    #s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    if save_txt:  # Write to file
+                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                        line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                        with open(txt_path + '.txt', 'a') as f:
+                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                    # if save_img :  # Add bbox to image
+                        # plot_one_box(xyxy, im0, line_thickness=3)
+
+            # Print time (inference)
+            print(f'{s}Done. ({t2 - t1:.3f}s)')
+            # show_seg_result(im0, (da_seg_mask,ll_seg_mask), is_demo=True)
+            polygon_segmentation = show_seg_result(im0, ([da_seg_mask]), is_demo=True)
+            frame_1D_arr = get_drivable_area_in_1D(polygon_segmentation)
+            
+
+            print(frame_1D_arr)
+            prev_frames.append(frame_1D_arr)
+            
+            max_length = max(len(frame) for frame in prev_frames)
+            prev_frames = [[f if f != 0 else 0 for f in frame] + [0] * (max_length - len(frame)) for frame in prev_frames]
+            # print("frame", prev_frames)
+
+            # Save results (image with detections)
+            if save_img:
+                if dataset.mode == 'image':
+                    cv2.imwrite(save_path, im0)
+                    print(f" The image with the result is saved in: {save_path}")
+                else:  # 'video' or 'stream'
+                    if vid_path != save_path:  # new video
+                        vid_path = save_path
+                        if isinstance(vid_writer, cv2.VideoWriter):
+                            vid_writer.release()  # release previous video writer
+                        if vid_cap:  # video
+                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                            #w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                            #h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                            w,h = im0.shape[1], im0.shape[0]
+                        else:  # stream
+                            fps, w, h = 30, im0.shape[1], im0.shape[0]
+                            save_path += '.mp4'
+                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                    vid_writer.write(im0)
+
+    for i in range(len(prev_frames) - 15):  # Iterate through all frames except the last 15
+        prev_10_frames.append(prev_frames[i:i+10])
+        next_5_frames.append(prev_frames[i+10:i+15])
+
+    # Convert prev_10_frames and next_5_frames into tensors
+    X = np.array(prev_10_frames)  # X.shape = (n, 10, 100)
+    y = np.array(next_5_frames)   # y.shape = (n, 5, 100)
+
+    np.save('X2.npy', X)
+    np.save('y2.npy', y)
+    # X = np.random.rand(500, 10, 100)
+    # y = np.random.rand(500, 5, 100)
+
+if __name__ == '__main__':
+    opt =  make_parser().parse_args()
+    print(opt)
+
+    with torch.no_grad():
+            detect()
